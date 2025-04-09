f690b43735761d238e9cbd53cd6a87cc14cdaad5
MalikL2005 <your_email@example.com>
2025-04-09 23:39:35 +0200

    feat: add mapping for go err not nil and copying of error message

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,11 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- Go Lang err != nil
+map({"n", "i"}, "<C-g>", "<ESC>oif err != nil {<ENTER>}<ESC>O")
+
+-- display and copy error message
+map({"n", "i"}, "<C-e>", "<ESC>:lua vim.diagnostic.open_float()<ENTER>:lua vim.diagnostic.open_float()<ENTER>jyy")
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
