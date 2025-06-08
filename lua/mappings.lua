2043bfbf63eb28376a0f94a20e2a215778b398e2
MalikL2005 <your_email@example.com>
2025-06-08 10:37:29 +0200

    feat: go to next err msg

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,10 +9,14 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- Go Lang err != nil
-map({"n", "i"}, "<C-g>", "<ESC>oif err != nil {<ENTER>}<ESC>O", {desc = "custom: go error is not nil"})
+map({"n", "i"}, "<C-g>", "<ESC>oif err != nil {<ENTER>}<ESC>O", {desc = "custom (GoLang): if error != nil"})
 
 -- display and copy error message
-map({"n", "i"}, "<C-e>", "<ESC>:lua vim.diagnostic.open_float()<ENTER>:lua vim.diagnostic.open_float()<ENTER>j", {desc = "custom: hover diagnostics"})
+map({"n", "i"}, "<C-m>", "<ESC>:lua vim.diagnostic.open_float()<ENTER>:lua vim.diagnostic.open_float()<ENTER>j", {desc = "custom (error): hover diagnostics"})
+
+-- go to next error message 
+map({"n", "i"}, "<C-e>", "<ESC>:lua vim.diagnostic.goto_next()<ENTER>", {desc = "custom (error): go to next error"})
+-- "<ESC>:lua vim.diagnostic.open_float()<ENTER>:lua vim.diagnostic.open_float()<ENTER>j"
 
 -- on neo tree: cd to parent
 map("n", "<leader>P", ":cd ..<ENTER>", {desc = "Neotree: move to parent"})
